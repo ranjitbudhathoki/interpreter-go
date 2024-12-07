bc5050fe924f9e82b15964d3d8137ead026a2171
RanjitBudhathoki <ranjit@2400-1A00-BD11-ip6.wlink.com.np>
2024-12-07 19:12:38 +0545

    define tokens

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ranjitbudhathoki/interpreter-go
+
+go 1.23.3
---
+++ token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENT = "IDENT" // identifiers like variable names
+	INT   = "INT"   //
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
